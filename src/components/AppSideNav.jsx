f7ba38238bf6735e6e43b5da3e15880d0abed9a7
PedroManriquez <ptrmanriquez@gmail.com>
2019-08-12 15:17:53 -0400

    Usando links de react router en sider

--- src/components/AppSideNav.jsx
+++ src/components/AppSideNav.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Layout, Menu, Icon } from 'antd';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 const { Sider } = Layout;
-const { SubMenu } = Menu;
 
 class AppSideNav extends React.Component {
   state = {
@@ -18,45 +18,22 @@ class AppSideNav extends React.Component {
     return (
       <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
         <div className="logo" />
-        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-          <Menu.Item key="1">
-            <Icon type="pie-chart" />
-            <span>Option 1</span>
-          </Menu.Item>
-          <Menu.Item key="2">
-            <Icon type="desktop" />
-            <span>Option 2</span>
-          </Menu.Item>
-          <SubMenu
-            key="sub1"
-            title={
-              <span>
-                <Icon type="user" />
-                <span>User</span>
-              </span>
-            }
-          >
-            <Menu.Item key="3">Tom</Menu.Item>
-            <Menu.Item key="4">Bill</Menu.Item>
-            <Menu.Item key="5">Alex</Menu.Item>
-          </SubMenu>
-          <SubMenu
-            key="sub2"
-            title={
-              <span>
-                <Icon type="team" />
-                <span>Team</span>
-              </span>
-            }
-          >
-            <Menu.Item key="6">Team 1</Menu.Item>
-            <Menu.Item key="8">Team 2</Menu.Item>
-          </SubMenu>
-          <Menu.Item key="9">
-            <Icon type="file" />
-            <span>File</span>
-          </Menu.Item>
-        </Menu>
+        <Router>
+          <Menu theme="dark" defaultSelectedKeys={['0']} mode="inline">
+            <Menu.Item key="0">
+              <Link to="/">
+                <Icon type="pie-chart" />
+                <span>Home</span>
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="1">
+              <Link to="/about">
+                <Icon type="desktop" />
+                <span>About</span>
+              </Link>
+            </Menu.Item>
+          </Menu>
+        </Router>
       </Sider>
     );
   }
